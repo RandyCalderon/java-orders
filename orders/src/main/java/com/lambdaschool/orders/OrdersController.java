9de27b749b2fb971b2916450ee87b97df2923ee3
Randy Calderon <randycweb@gmail.com>
2019-02-20 23:26:36 -0800

    Added customers get,post,put,delete

--- orders/src/main/java/com/lambdaschool/orders/OrdersController.java
+++ orders/src/main/java/com/lambdaschool/orders/OrdersController.java
@@ -6,13 +6,14 @@ import com.lambdaschool.orders.models.Order;
 import com.lambdaschool.orders.repository.AgentRepository;
 import com.lambdaschool.orders.repository.CustomerRepository;
 import com.lambdaschool.orders.repository.OrderRepository;
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = {}, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -28,20 +29,60 @@ public class OrdersController {
     OrderRepository ordrepos;
 
     // Agent
-    @GetMapping("/agent")
+    @GetMapping("/agents")
     public List<Agent> findagent() {
         return agentrepos.findAll();
     }
 
     // Customer
-    @GetMapping("/customer")
+    @GetMapping("/customers")
     public List<Customer> findcust() {
         return custrepos.findAll();
     }
 
+    @GetMapping("/customers/custcode/{custcode}")
+    public Customer findcustId(@PathVariable long custcode) {
+        var foundCust = custrepos.findById(custcode);
+        if (foundCust.isPresent()) {
+            return foundCust.get();
+        } else {
+            return null;
+        }
+    }
+
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer) throws URISyntaxException {
+        return custrepos.save(customer);
+    }
+
+    @PutMapping("/customers/custocode/{custcode}")
+    public Customer changeCust(@RequestBody Customer newcust, @PathVariable long custcode) throws URISyntaxException {
+        Optional<Customer> updateCust = custrepos.findById(custcode);
+        if (updateCust.isPresent()) {
+           newcust.setId(custcode);
+           custrepos.save(newcust);
+
+           return newcust;
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/customers/custcode/{custcode}")
+    public Customer deleteCustomer(@PathVariable long custcode) {
+        var foundCust = custrepos.findById(custcode);
+        if (foundCust.isPresent()) {
+            custrepos.deleteById(custcode);
+            return foundCust.get();
+        } else {
+            return null;
+        }
+    }
+
     // Order
-    @GetMapping("/order")
+    @GetMapping("/orders")
     public List<Order> findorder() {
         return ordrepos.findAll();
     }
+
 }
